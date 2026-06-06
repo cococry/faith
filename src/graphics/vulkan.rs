17c6b93f1d362d56ad1e0f44f679486f56e1af10
Cococry <cococry.btw@gmail.com>
2026-06-07 00:27:32 +0200

    vulkan swapchain implementation

--- src/graphics/vulkan.rs
+++ src/graphics/vulkan.rs
@@ -1,13 +1,13 @@
 use std::ffi::{CStr, CString, c_char};
+use std::ptr::null;
 
 use crate::graphics::device::{DrawIndexedInstanced, TextureArrayDesc, TextureKind};
-use crate::platform::window::WindowHandleInfo;
-use crate::platform::{self, Platform};
+use crate::platform::{Platform};
 use crate::graphics::{BufferDesc, BufferHandle, Color, DrawIndexed, GraphicsDevice, PipelineDesc, PipelineHandle, TextureDesc, TextureHandle};
 
-use ash::vk::{QueueFlags, SurfaceFormatKHR};
-use ash::{Entry, khr, vk};
-use x11::keysym::XK_Pointer_EnableKeys;
+use ash::vk::{Extent2D, ImageViewCreateInfo, QueueFlags };
+use ash::{Entry, vk};
+use vk_mem::{Alloc, AllocationCreateInfo};
 
 pub struct VulkanRenderer {
     instance: ash::Instance,
@@ -30,9 +30,10 @@ pub struct VulkanRenderer {
     graphics_queue_family_idx: usize,
     present_queue_family_idx: usize,
 
-    swapchain_info: SwapchainInfo,
+    swapchain: Swapchain,
 }
 
+#[derive(Default)]
 struct SwapchainInfo {
     present_modes: Vec<vk::PresentModeKHR>,
     surface_fmts: Vec<vk::SurfaceFormatKHR>,
@@ -40,6 +41,24 @@ struct SwapchainInfo {
 
     ideal_present_mode: vk::PresentModeKHR,
     ideal_surface_fmt: vk::SurfaceFormatKHR,
+    ideal_depth_fmt: vk::Format,
+}
+
+#[derive(Default)]
+struct Swapchain {
+    img_count: u32,
+        
+    info: SwapchainInfo,
+
+    handle: vk::SwapchainKHR, 
+    images: Vec<vk::Image>,
+    extent: Extent2D, 
+
+    img_views: Vec<vk::ImageView>,
+    img_views_depth: Vec<vk::ImageView>,
+    depth_images: Vec<vk::Image>,
+    depth_image_allocs: Vec<vk_mem::Allocation>,
+    imgs_in_flight: Vec<vk::Fence>,
 }
 
 impl VulkanRenderer {
@@ -144,13 +163,12 @@ impl VulkanRenderer {
             queue for graphics or presentation is missing.");
     }
 
-    fn string_vec_to_raw_bytes(
+    fn string_vec_to_cstrings(
         vec: &Vec<String> 
-    ) -> Vec<*const c_char> {
-       vec 
-            .iter()
-            .map(|c_str| c_str.as_ptr() as *const i8)
-            .collect::< Vec<*const i8>>()
+    ) -> Vec<CString> {
+        vec.iter()
+            .map(|s| CString::new(s.as_str()).expect("String contains Nullbyte"))
+            .collect()
     }
 
     fn create_logical_device(
@@ -178,11 +196,16 @@ impl VulkanRenderer {
 
         let enabled_exts_vec: Vec<String> = vec!["VK_KHR_swapchain".to_string()];
        
-        let enabled_exts = Self::string_vec_to_raw_bytes(&enabled_exts_vec);
+        let enabled_exts = Self::string_vec_to_cstrings(&enabled_exts_vec);
+
+        let enabled_exts_final: Vec<*const c_char> = enabled_exts 
+            .iter()
+            .map(|cs| cs.as_ptr())
+            .collect();
 
         let device_info = vk::DeviceCreateInfo {
             enabled_extension_count: 1, 
-            pp_enabled_extension_names: enabled_exts.as_ptr(),
+            pp_enabled_extension_names: enabled_exts_final.as_ptr(), 
             p_queue_create_infos: queues.as_ptr(),
             queue_create_info_count: queues.len() as u32,
             ..Default::default()
@@ -226,27 +249,249 @@ impl VulkanRenderer {
         vk::PresentModeKHR::FIFO
     }
 
+    fn get_swapchain_extent(
+        swap_info: &SwapchainInfo, 
+        width: u32, 
+        height: u32, 
+    ) -> Extent2D {
+        if swap_info.capabilities.current_extent.width != u32::MAX {
+            return swap_info.capabilities.current_extent;
+        }
+
+        let mut extent = Extent2D {
+            width, height
+        };
+
+        extent.width    = std::cmp::min(width, 
+            swap_info.capabilities.max_image_extent.width); 
+        extent.height   = std::cmp::min(width, 
+            swap_info.capabilities.max_image_extent.height); 
+
+        extent.width    = std::cmp::max(width, 
+            swap_info.capabilities.min_image_extent.width); 
+        extent.height   = std::cmp::max(width, 
+            swap_info.capabilities.min_image_extent.height); 
+
+        extent
+    }
+
     fn get_swapchain_info(
-        phys_dev: vk::PhysicalDevice,
+        instance: &ash::Instance,
+        phys_dev: &vk::PhysicalDevice,
         surface_inst: &ash::khr::surface::Instance,
         surface: &vk::SurfaceKHR,
     ) -> anyhow::Result<SwapchainInfo> {
-        let capabilities    = unsafe { surface_inst.get_physical_device_surface_capabilities(phys_dev, *surface)? };
-        let surface_fmts    = unsafe { surface_inst.get_physical_device_surface_formats(phys_dev, *surface)? };
-        let present_modes   = unsafe { surface_inst.get_physical_device_surface_present_modes(phys_dev, *surface)? };
+        let capabilities    = unsafe { surface_inst.get_physical_device_surface_capabilities(*phys_dev, *surface)? };
+        let surface_fmts    = unsafe { surface_inst.get_physical_device_surface_formats(*phys_dev, *surface)? };
+        let present_modes   = unsafe { surface_inst.get_physical_device_surface_present_modes(*phys_dev, *surface)? };
       
-        let ideal_present_mode = Self::get_ideal_swapchain_present_mode(&present_modes);
-        let ideal_surface_fmt  = Self::get_ideal_swapchain_surface_fmt(&surface_fmts);
+        let ideal_present_mode  = Self::get_ideal_swapchain_present_mode(&present_modes);
+        let ideal_surface_fmt   = Self::get_ideal_swapchain_surface_fmt(&surface_fmts);
+        let ideal_depth_fmt     = Self::get_ideal_swapchain_depth_fmt(instance, phys_dev);
 
-        Ok(SwapchainInfo { present_modes, surface_fmts, capabilities,
-            ideal_present_mode, ideal_surface_fmt})
+        Ok(SwapchainInfo { 
+            present_modes, 
+            surface_fmts, 
+            capabilities,
+            ideal_present_mode, 
+            ideal_surface_fmt, 
+            ideal_depth_fmt
+        })
+    }
+
+    fn get_ideal_swapchain_depth_fmt(
+        instance: &ash::Instance, 
+        phys_dev: &vk::PhysicalDevice,
+    ) -> vk::Format {
+        let candidates: [vk::Format; 4] = [
+            vk::Format::D32_SFLOAT,
+            vk::Format::D32_SFLOAT_S8_UINT,
+            vk::Format::D24_UNORM_S8_UINT,
+            vk::Format::D16_UNORM,
+        ];
+
+        for candidate in candidates {
+            let props = unsafe { 
+                instance.get_physical_device_format_properties(
+                    *phys_dev, candidate) 
+            };
+
+            if props.optimal_tiling_features.contains(
+                vk::FormatFeatureFlags::DEPTH_STENCIL_ATTACHMENT) {
+                return candidate;
+            }
+        }
+
+        vk::Format::UNDEFINED
+    }
+
+    fn get_depth_image_mask(
+        fmt: vk::Format
+    ) -> vk::ImageAspectFlags {
+        match fmt {
+            vk::Format::D32_SFLOAT_S8_UINT 
+                | vk::Format::D24_UNORM_S8_UINT =>
+                return vk::ImageAspectFlags::DEPTH | vk::ImageAspectFlags::STENCIL, 
+
+            _ => return vk::ImageAspectFlags::DEPTH 
+        }
     }
 
     fn create_swapchain(
-        phys_dev: vk::PhysicalDevice,
-        swap_info: &SwapchainInfo,
-    ) -> anyhow::Result<vk::SwapchainKHR> {
-        todo!()
+        instance: &ash::Instance,
+        swapchain: &mut Swapchain,
+        swapchain_dev: &ash::khr::swapchain::Device,
+        allocator: vk_mem::Allocator,
+
+        surface: &vk::SurfaceKHR,
+        surface_inst: &ash::khr::surface::Instance,
+
+        logical_dev: &ash::Device,
+        phys_dev: &vk::PhysicalDevice,
+        width: u32,
+        height: u32,
+
+        graphics_queue_family_idx: isize
+        , present_queue_family_idx: isize
+    ) -> anyhow::Result<()> {
+
+        tracing::info!("Creating Vulkan swapchain");
+
+        swapchain.info = Self::get_swapchain_info(instance, phys_dev, surface_inst, surface)?;
+
+        if swapchain.info.ideal_depth_fmt == vk::Format::UNDEFINED {
+            anyhow::bail!("No supported Vulkan depth format found for application window swapchain.");
+        }
+
+        swapchain.img_views.clear();
+        swapchain.img_views_depth.clear();
+        swapchain.images.clear();
+        swapchain.imgs_in_flight.clear();
+        swapchain.depth_images.clear();
+
+        let extent = Self::get_swapchain_extent(&swapchain.info, 
+            width, height);
+
+        let img_count = std::cmp::min(
+            swapchain.info.capabilities.max_image_count,
+            swapchain.img_count);
+
+        let mut create_info = vk::SwapchainCreateInfoKHR {
+            surface: *surface,
+            min_image_count: img_count,
+            image_color_space:   swapchain.info.ideal_surface_fmt.color_space,
+            image_array_layers: 1,
+            image_usage: 
+                vk::ImageUsageFlags::COLOR_ATTACHMENT | 
+                vk::ImageUsageFlags::TRANSFER_DST,
+                image_extent : extent, 
+                pre_transform: swapchain.info.capabilities.current_transform,
+                composite_alpha: vk::CompositeAlphaFlagsKHR::OPAQUE,
+                present_mode: swapchain.info.ideal_present_mode,
+                clipped: vk::TRUE, 
+                ..Default::default()
+        };
+
+        let families = vec![graphics_queue_family_idx as u32, present_queue_family_idx as u32];
+
+        if graphics_queue_family_idx != present_queue_family_idx {
+            create_info.image_sharing_mode = vk::SharingMode::CONCURRENT;
+            create_info.queue_family_index_count = 2;
+            create_info.p_queue_family_indices = families.as_ptr();
+        } else {
+            create_info.image_sharing_mode = vk::SharingMode::EXCLUSIVE;
+            create_info.queue_family_index_count = 0;
+            create_info.p_queue_family_indices = null()  
+        }
+
+        let swapchain_handle = unsafe { swapchain_dev.create_swapchain(
+            &create_info, None)? };
+
+        let images = unsafe { swapchain_dev.get_swapchain_images(swapchain_handle)? };
+
+        swapchain.img_views.reserve(images.len());
+        swapchain.img_views_depth.reserve(images.len());
+        swapchain.imgs_in_flight.reserve(images.len());
+        swapchain.depth_images.reserve(images.len());
+
+        swapchain.images = images;
+        swapchain.extent = extent; 
+
+        for image in swapchain.imgs_in_flight.iter_mut() {
+            *image = vk::Fence::null();
+        }
+
+        for (idx, _) in swapchain.images.iter().enumerate() {
+            let depth_image_info = vk::ImageCreateInfo {
+                image_type: vk::ImageType::TYPE_2D,
+                format: swapchain.info.ideal_depth_fmt,
+                extent: vk::Extent3D { width, height, depth: 1 },
+                mip_levels: 1,
+                array_layers: 1,
+                samples: vk::SampleCountFlags::TYPE_1,
+                tiling: vk::ImageTiling::OPTIMAL,
+                usage: vk::ImageUsageFlags::DEPTH_STENCIL_ATTACHMENT,
+                sharing_mode: vk::SharingMode::EXCLUSIVE,
+                initial_layout: vk::ImageLayout::UNDEFINED,
+
+                ..Default::default()
+            };
+
+            let depth_alloc_info = AllocationCreateInfo {
+                ..Default::default()
+            };
+
+            (swapchain.depth_images[idx], swapchain.depth_image_allocs[idx]) =  
+                unsafe { allocator.create_image(&depth_image_info, &depth_alloc_info)? }; 
+
+            let image_view_info = ImageViewCreateInfo {
+                image: swapchain.images[idx],
+                view_type: vk::ImageViewType::TYPE_2D,
+                format: swapchain.info.ideal_surface_fmt.format,
+                components: vk::ComponentMapping { 
+                    r: vk::ComponentSwizzle::IDENTITY,
+                    g: vk::ComponentSwizzle::IDENTITY,
+                    b: vk::ComponentSwizzle::IDENTITY,
+                    a: vk::ComponentSwizzle::IDENTITY,
+                }, 
+                subresource_range: vk::ImageSubresourceRange { 
+                    aspect_mask: vk::ImageAspectFlags::COLOR, 
+                    base_mip_level: 0,
+                    level_count: 1,
+                    base_array_layer: 0,
+                    layer_count: 1,
+                },
+                ..Default::default()
+            };
+            let depth_image_view_info = ImageViewCreateInfo {
+                image: swapchain.depth_images[idx],
+                view_type: vk::ImageViewType::TYPE_2D, 
+                format: swapchain.info.ideal_depth_fmt,
+
+                subresource_range: vk::ImageSubresourceRange { 
+                    aspect_mask: Self::get_depth_image_mask(
+                                     swapchain.info.ideal_depth_fmt), 
+                    base_mip_level: 0,
+                    level_count: 1,
+                    base_array_layer: 0,
+                    layer_count: 1,
+                },
+                ..Default::default()
+            };
+
+            swapchain.img_views[idx]        = unsafe { 
+                logical_dev.create_image_view(&image_view_info, None)? 
+            };
+            swapchain.img_views_depth[idx]  = unsafe { 
+                logical_dev.create_image_view(&depth_image_view_info, None)? 
+            };
+        }
+        
+        tracing::info!("Initialized Vulkan swapchain (width: {}, height: {})",
+            width, height);
+
+        Ok(())
+
     }
 
     pub fn new(platform: &Platform) -> anyhow::Result<Self> { 
@@ -314,9 +559,26 @@ impl VulkanRenderer {
 
         let (logical_device, graphics_queue, present_queue) = 
             Self::create_logical_device(&instance, &phys_dev, present_queue_family_idx, graphics_queue_family_idx)?;
+            
+        let allocator = unsafe { vk_mem::Allocator::new(vk_mem::AllocatorCreateInfo::new(&instance, &logical_device, phys_dev))? };
 
-        let swapchain_info = Self::get_swapchain_info(phys_dev, &surface_inst, &surface)?;
-        
+        let mut swapchain  = Swapchain::default();
+        let swapchain_dev = ash::khr::swapchain::Device::new(&instance, &logical_device);
+
+        Self::create_swapchain(&instance, 
+            &mut swapchain, 
+            &swapchain_dev, 
+            allocator, 
+            &surface, 
+            &surface_inst, 
+            &logical_device, 
+            &phys_dev, 
+            1280, 
+            720,
+            graphics_queue_family_idx,
+            present_queue_family_idx)?;
+
+
         Ok(Self {
             instance,
             logical_device,
@@ -338,7 +600,7 @@ impl VulkanRenderer {
             graphics_queue_family_idx: graphics_queue_family_idx as usize,
             present_queue_family_idx: present_queue_family_idx as usize,
 
-            swapchain_info,
+            swapchain,
             
         })
     }
